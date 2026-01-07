2cda55166b254af5bba6c33cdd8c049748857cd7
ace <ace7coderr@gmail.com>
2026-01-07 09:24:03 +0100

    Initial commit

--- src/app/supabaseClient.ts
+++ src/app/supabaseClient.ts
@@ -1,8 +1,6 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js"
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl: string = process.env.SUPABASE_URL || "";
-const supabaseAnonKey: string = process.env.SUPABASE_URL || "";
-
-const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
-
-export default supabase;
+export const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
